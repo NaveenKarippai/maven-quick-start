5be075b103fc17fec63c4d8df4e14e4cc31b7231
NaveenKarippai <naveenranjankarippai@gmail.com>
2018-07-09 20:24:58 +0200

    Add Junit dependency on maven

---
+++ src/test/java/com/naveenkarippai/ApplicationTest.java
@@ -0,0 +1,16 @@
+package com.naveenkarippai;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+    private Application app;
+    
+    @Before
+    public void setup() {
+        app = new Application();
+    }
+    
+}
